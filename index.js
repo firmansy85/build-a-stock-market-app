bbd79e6012f217ace86f5e7a9f6067480264fa0e
firmansy85 <firman.ibnurahman@gmail.com>
2023-08-27 19:18:46 +0700

    inital commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//set handlebars middleware
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+//const otherstuff = "hello there, this is other stuff";
+
+//set handlebars routes
+app.get('/', function (req, res) {
+    res.render('home', {
+        stuff: "This is stuff..."
+        //stuff: otherstuff
+    });
+});
+
+// create static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on Port ' + PORT));
