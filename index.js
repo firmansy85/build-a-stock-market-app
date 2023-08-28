a55b8a77d2a13a2d723980452402fd9478acfc9f
firmansy85 <firman.ibnurahman@gmail.com>
2023-08-28 12:24:47 +0700

    version 1.0

--- index.js
+++ index.js
@@ -4,8 +4,24 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+//api key pk_15b6dc09b48142ac8ff3496021bb80f4 -> copy dari iexclod
+function call_api(finishedAPI, ticker) {
+request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_15b6dc09b48142ac8ff3496021bb80f4', { json: true }, (err, res, body) => {
+    if (err) {return console.log(err);}
+    if (res.statusCode === 200){
+        //console.log(body);
+        finishedAPI(body);
+    };
+  });
+};
 
 //set handlebars middleware
 app.engine('handlebars', exphbs.engine());
@@ -13,12 +29,31 @@ app.set('view engine', 'handlebars');
 
 //const otherstuff = "hello there, this is other stuff";
 
-//set handlebars routes
+//set handlebars index GET routes
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: "This is stuff..."
-        //stuff: otherstuff
-    });
+    call_api(function(doneAPI) {
+           res.render('home', {
+        //stuff: "This is stuff..."
+           stock: doneAPI
+        });
+    }, "fb");
+});
+
+
+//set handlebars index POST routes
+app.post('/', function (req, res) {
+    call_api(function(doneAPI) {
+           //posted_stuff = req.body.stock_ticker;
+           res.render('home', {
+           stock: doneAPI,
+           //posted_stuff: posted_stuff
+        });
+    }, req.body.stock_ticker);
+});
+
+//about routes
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // create static folder
